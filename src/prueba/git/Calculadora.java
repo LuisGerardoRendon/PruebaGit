c3278156fbdfe5cef99a9a5e4c2d555aab3ccf82
rendo <rendo@DESKTOP-0FGPCGK>
2020-05-09 13:29:17 -0500

    Calse calculadora

---
+++ src/prueba/git/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prueba.git;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Calculadora {
+    public int suma(int a, int b){
+        return a + b;
+    }
+
+}
